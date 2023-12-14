cbb42c73e88e7f97f3bd421975605da799aecfd3
Igor Morozov <igor.morozov@ringteam.com>
2023-12-15 01:39:36 +0200

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Predicting Wine Class with batching")
 with open("models/wine.pkl", "rb") as file:
     clf = pickle.load(file)
 
-
+# bla
 class Wine(BaseModel):
     batches: List[conlist(item_type=float, min_length=13, max_length=13)]
 
